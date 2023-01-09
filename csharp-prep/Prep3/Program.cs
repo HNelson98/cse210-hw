18c08994a63a59a6e9afdca3218c350351b206b7
HNelson98 <Henry.nelson89@gmail.com>
2023-01-09 09:58:13 -0700

    Finished Prep3 with all stretch

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,46 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+        static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        bool play = true;
+        while (play == true)
+        {
+            Random randomNumber = new Random();
+            int number = randomNumber.Next(1,101);
+            
+            Console.WriteLine($"Welcom to Hilow!");
+
+            int guesses = 0;
+
+            bool guessed = false;
+            while (guessed == false) 
+            {
+            Console.Write("Enter your guess: ");
+            string guessString = Console.ReadLine();
+            int guess = int.Parse(guessString);
+
+            
+            guesses += 1;
+            
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            } else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            } else
+            {
+                Console.WriteLine($"You guessed it in {guesses} guesses");
+                guessed = true;
+            }
+            }
+            Console.Write("Would you like to play again? y/n: ");
+            string again = Console.ReadLine();
+            if (again == "n")
+            {
+                play = false;
+            }
+        }
+    } 
 }
