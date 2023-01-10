69a131628e26b2c075c2e290023852fe4e7090dc
Henry Nelson <60790820+HNelson98@users.noreply.github.com>
2023-01-10 12:34:31 -0700

    Finished Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+
+        bool done = false;
+        while (!done)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            if (input == "0")
+            {
+                done = true;
+            } else
+            {
+                int number = int.Parse(input);
+                numbers.Add(number);
+            }
+        }
+
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+        Console.WriteLine($"The total is: {total}");
+
+        int average = total / numbers.Count();
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers.Max();
+        
+        Console.WriteLine($"The highest number is: {max}");
+
+
+
     }
 }
