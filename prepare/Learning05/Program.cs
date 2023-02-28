38009e67ab0681f867f8cddba6195f7766765468
Henry Nelson <60790820+HNelson98@users.noreply.github.com>
2023-02-27 18:34:57 -0700

    Finished the learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Circle("red", 2));
+        shapes.Add(new Rectangle("blue", 2, 3));
+        shapes.Add(new Square("green", 2));
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor() + " " + shape.GetArea());
+        }
     }
 }
